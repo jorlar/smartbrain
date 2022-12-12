4a3e569a02da62623048ce3a6e1bef231ad15c9f
jorlar <jqern.larsen@gmail.com>
2022-12-12 11:57:04 +0100

    :sparkles: Add inputform and button with CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/imageLinkForm';
+import Rank from './components/Rank/Rank'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-    
+      <Rank />
+      <ImageLinkForm/>
     </div>
   );
 }
--- src/components/Logo/logo.js
+++ src/components/Logo/logo.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import brain from "./brain.png";
+import "./logo.css";
 
 const Logo = () => {
   return (
-    <div>logo</div>
-  )
-}
+    <div className="ma4 mt0">
+      <Tilt className="tilt">
+        <div className="tilt pa3">
+          <img
+            style={{ paddingTop: "5px", height: "100px" }}
+            src={brain}
+            alt="logo"
+          />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
